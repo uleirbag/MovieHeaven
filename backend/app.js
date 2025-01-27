6d9677a5abc540e2e65d317a89df3733b9263747
uleirbag <uleirbag@movieheaven.com>
2025-01-27 15:19:50 +0200

    Initialize basic Express project with essentials

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: '../.env' });
+
+const express = require('express');
+const httpLogger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(httpLogger('dev'));
+app.use(cors()) 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Server-ul Express a pornit cu succes :D')
+});
+
+app.listen(port, () => {
+  console.log(`Serverul ruleaza pe portul ${port}!`)
+});
